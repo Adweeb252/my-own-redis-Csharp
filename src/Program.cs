0e2f1fe7b64f3b996f2355c29a311bf385e69eb9
Adweeb252 <deviladir1p@gmail.com>
2025-04-02 18:14:37 +0530

    implemented ping and echo command

--- src/Program.cs
+++ src/Program.cs
@@ -6,13 +6,51 @@ using System.Text;
 Console.WriteLine("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
-TcpListener server = new TcpListener(IPAddress.Any, 6379);
+TcpListener server = new TcpListener(IPAddress.Any, 6380);
 server.Start();
-var client = server.AcceptSocket(); // wait for client
-Console.WriteLine("Client connected at " + client.RemoteEndPoint);
-client.Send(Encoding.UTF8.GetBytes("+PONG\r\n"));
-byte[] buffer = new byte[1024];
-int bytesRead = client.Receive(buffer);
-string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
-Console.WriteLine($"Received: {message}");
-// client.SendAsync(Encoding.UTF8.GetBytes("+PONG\r\n"), SocketFlags.None);
+bool isServerRunning = true;
+
+while (isServerRunning)
+{
+    var acceptTask = server.AcceptSocketAsync(); // wait for client
+    var client = await acceptTask;
+    Console.WriteLine($"Client connected at {client.RemoteEndPoint}");
+    Task.Run(() => handleClient(client)); // handle client in a separate thread
+
+}
+void handleClient(Socket client)
+{
+    while (true)
+    {
+        byte[] buffer = new byte[1024];
+        int bytesRead = client.Receive(buffer);
+        if (bytesRead == 0)
+            return; // client disconnected
+
+        string message = Encoding.UTF8.GetString(buffer, 0, bytesRead).Trim();
+        var command = message.Split("\r\n", StringSplitOptions.RemoveEmptyEntries);
+        string cmdsize = command[0].Substring(1);
+        int argsize = int.Parse(cmdsize);
+        string cmd = command[2].ToUpper();
+        string response = "none";
+        if (cmd == "PING")
+        {
+            response = "+PONG\r\n";
+        }
+        else if (cmd == "ECHO" && argsize > 1)
+        {
+            string echo = "";
+            for (int i = 4; i < command.Length; i += 2)
+            {
+                echo += command[i] + " ";
+            }
+            response = $"+{echo}\r\n";
+        }
+        else
+        {
+            response = "-ERR unknown command\r\n";
+        }
+        client.Send(Encoding.UTF8.GetBytes(response));
+        Console.WriteLine($"Sent: {response}");
+    }
+}
