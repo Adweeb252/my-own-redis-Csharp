689973ee65260195a8337f918aee3b89f7da3ea0
Adweeb252 <deviladir1p@gmail.com>
2025-04-02 11:41:18 +0530

    initial setup

---
+++ src/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+// You can use print statements as follows for debugging, they'll be visible when running tests.
+Console.WriteLine("Logs from your program will appear here!");
+
+// Uncomment this block to pass the first stage
+TcpListener server = new TcpListener(IPAddress.Any, 6379);
+server.Start();
+var client = server.AcceptSocket(); // wait for client
+Console.WriteLine("Client connected at " + client.RemoteEndPoint);
+client.Send(Encoding.UTF8.GetBytes("+PONG\r\n"));
+byte[] buffer = new byte[1024];
+int bytesRead = client.Receive(buffer);
+string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+Console.WriteLine($"Received: {message}");
+// client.SendAsync(Encoding.UTF8.GetBytes("+PONG\r\n"), SocketFlags.None);
