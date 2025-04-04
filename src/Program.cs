4af359c6812e544a7935b2da5cd0c823cef05148
Adweeb252 <deviladir1p@gmail.com>
2025-04-04 12:32:10 +0530

    format changes

--- src/Program.cs
+++ src/Program.cs
@@ -50,70 +50,72 @@ void handleClient(Socket client)
 
         string message = Encoding.UTF8.GetString(buffer, 0, bytesRead).Trim();
         var command = message.Split("\r\n", StringSplitOptions.RemoveEmptyEntries);
-        string cmdsize = command[0].Substring(1);
+        handleCommands(command, client);
+    }
+}
+void handleCommands(string[] command, Socket client)
+{
+    string cmdsize = command[0].Substring(1);
 
-        int argsize = int.Parse(cmdsize); //arguement size which is the first line
-
-        string cmd = command[2].ToUpper();
-        string response = "none";
-
-        if (cmd == "SET" && argsize >= 3)
+    int argsize = int.Parse(cmdsize); //arguement size which is the first line
+    string cmd = command[2].ToUpper();
+    string response = "none";
+    if (cmd == "SET" && argsize >= 3)
+    {
+        string key = command[4];
+        string val = command[6];
+        if (argsize == 5)
         {
-            string key = command[4];
-            string val = command[6];
-            if (argsize == 5)
-            {
-                int expt = int.Parse(command[10]);
-                db.Set(key, (object)val, DateTimeOffset.Now.AddMilliseconds(expt));
-            }
-            else
-            {
-                db.Set(key, (object)val, DateTimeOffset.MaxValue);
-            }
-            // dict[key] = val;
-            response = "+OK\r\n";
-        }
-        else if (cmd == "GET" && argsize == 2)
-        {
-            string key = command[4];
-            if (db[key] != null)
-            {
-                response = $"+{db[key]}\r\n";
-            }
-            else
-            {
-                response = "$-1\r\n";
-            }
-        }
-        else if (cmd == "PING")
-        {
-            response = "+PONG\r\n";
-        }
-        else if (cmd == "ECHO" && argsize > 1)
-        {
-            string echo = "";
-            for (int i = 4; i < command.Length; i += 2)
-            {
-                echo += command[i] + " ";
-            }
-            response = $"+{echo}\r\n";
-        }
-        else if (cmd == "CONFIG" && command[4].ToUpper() == "GET" && argsize == 3)
-        {
-            if (command[6].Equals("dir"))
-            {
-                response = $"*2\r\n$3\r\ndir\r\n${dir.Length}\r\n{dir}\r\n";
-            }
-            else if (command[6].Equals("dbfilename"))
-            {
-                response = $"*2\r\n$3\r\ndir\r\n${dbFilename.Length}\r\n{dbFilename}\r\n";
-            }
+            int expt = int.Parse(command[10]);
+            db.Set(key, (object)val, DateTimeOffset.Now.AddMilliseconds(expt));
         }
         else
         {
-            response = "-ERR unknown command\r\n";
+            db.Set(key, (object)val, DateTimeOffset.MaxValue);
         }
-        client.Send(Encoding.UTF8.GetBytes(response));
-        Console.WriteLine($"Sent: {response}");
+        // dict[key] = val;
+        response = "+OK\r\n";
     }
+    else if (cmd == "GET" && argsize == 2)
+    {
+        string key = command[4];
+        if (db[key] != null)
+        {
+            response = $"+{db[key]}\r\n";
+        }
+        else
+        {
+            response = "$-1\r\n";
+        }
+    }
+    else if (cmd == "PING")
+    {
+        response = "+PONG\r\n";
+    }
+    else if (cmd == "ECHO" && argsize > 1)
+    {
+        string echo = "";
+        for (int i = 4; i < command.Length; i += 2)
+        {
+            echo += command[i] + " ";
+        }
+        response = $"+{echo}\r\n";
+    }
+    else if (cmd == "CONFIG" && command[4].ToUpper() == "GET" && argsize == 3)
+    {
+        if (command[6].Equals("dir"))
+        {
+            response = $"*2\r\n$3\r\ndir\r\n${dir.Length}\r\n{dir}\r\n";
+        }
+        else if (command[6].Equals("dbfilename"))
+        {
+            response = $"*2\r\n$3\r\ndir\r\n${dbFilename.Length}\r\n{dbFilename}\r\n";
+        }
+    }
+    else
+    {
+        response = "-ERR unknown command\r\n";
+    }
+    client.Send(Encoding.UTF8.GetBytes(response));
+    Console.WriteLine($"Sent: {response}");
 }
